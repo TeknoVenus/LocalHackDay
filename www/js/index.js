be3bd7d38a925a767eb63cafd585e368e17712e6
Stephen Foulds <stephen.foulds@gmail.com>
2016-12-03 12:03:24 +0000

    Read value from NFC tag (text/simon)

--- www/js/index.js
+++ www/js/index.js
@@ -18,7 +18,7 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -26,67 +26,26 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         function failure(reason) {
             alert("There was a problem - " + reason);
         }
 
-        nfc.addNdefListener(
+        nfc.addMimeTypeListener(
+            'text/simon',
             app.onNdef,
-            function() {
-                console.log("Listening for NDEF tags.");
+            function () {
+                console.log("Listening for NDEF mime tags with type text/simon.");
             },
             failure
         );
 
-        if (device.platform == "Android") {
-            // Android reads non-NDEF tag. BlackBerry and Windows don't.
-            nfc.addTagDiscoveredListener(
-                app.onNfc,
-                function() {
-                    console.log("Listening for non-NDEF tags.");
-                },
-                failure
-            );
-
-            // Android launches the app when tags with mime type text/pg are scanned
-            // because of an intent in AndroidManifest.xml.
-            // phonegap-nfc fires an ndef-mime event (as opposed to an ndef event)
-            // the code reuses the same onNfc handler
-            nfc.addMimeTypeListener(
-                'text/pg',
-                app.onNdef,
-                function() {
-                    console.log("Listening for NDEF mime tags with type text/pg.");
-                },
-                failure
-            );
-        }
-    },
-
-    onNfc: function (nfcEvent) {
-
-        var tag = nfcEvent.tag;
-
-        alert("NFC");
-        console.log(JSON.stringify(nfcEvent.tag));
-        navigator.notification.vibrate(100);
     },
     onNdef: function (nfcEvent) {
-        console.log(JSON.stringify(nfcEvent.tag));
-        var tag = nfcEvent.tag;
-
-        // BB7 has different names, copy to Android names
-        if (tag.serialNumber) {
-            tag.id = tag.serialNumber;
-            tag.isWritable = !tag.isLocked;
-            tag.canMakeReadOnly = tag.isLockable;
-        }
-
-        alert("NDEF");
-        alert(app.tagTemplate(tag));
-
-        navigator.notification.vibrate(100);
+        navigator.vibrate(300);
+        console.log(JSON.stringify(nfcEvent));
+        var tagData = nfcEvent.tag;
+        document.querySelector("#nfc").innerText = nfc.bytesToString(nfcEvent.tag.ndefMessage[0].payload);
     }
 };
 
